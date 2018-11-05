a035a7d115d37378fd6e628831c3dcf260fbdebc
Christopher Boo <christopherboo66@gmail.com>
2018-11-05 17:36:44 +0800

    add naive bfs for checking purpose

---
+++ truth.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 305;
+
+int n, m, u, v, res[MAXN][MAXN];
+vector<int> adjList[MAXN];
+
+int main() {
+
+	cin >> n >> m;
+	while (m--) {
+		cin >> u >> v;
+
+		adjList[u].push_back(v);
+		adjList[v].push_back(u);
+	}
+
+	clock_t start = clock();
+
+	memset(res, -1, sizeof(res));
+
+	for (int i = 0; i < n; i++) {
+		queue<int> q; q.push(i); res[i][i] = 0;
+		while (!q.empty()) {
+			u = q.front(); q.pop();
+			for (int v : adjList[u]) {
+				if (res[i][v] == -1) {
+					q.push(v); res[i][v] = res[i][u] + 1;
+				}
+			}
+		}
+	}
+	
+	double duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+
+	printf("duration: %f\n", duration);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", res[i][j]);
+		} printf("\n");
+	}
+
+	return 0;
+}
